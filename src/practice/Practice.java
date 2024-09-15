287a1228f56455f5cb29ad3f8fb5a3ee5d5e8ca5
pritam <pritam@LAPTOP-6FQ0R5J5>
2024-09-15 17:57:19 +0530

    var change in practice.java

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -2,11 +2,11 @@ package practice;
 
 public class Practice {
 	
-	static int a=10;
+	static int b=10;
 
 	public static void main(String[] args) {
-		a=20;
-		System.out.println(a);
+		b=20;
+		System.out.println(b);
 
 	}
 
