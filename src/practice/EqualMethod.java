e8240b2edc0312fb745c4e4bcb17a16287a9d571
pritam <pritam@LAPTOP-6FQ0R5J5>
2024-09-15 16:54:04 +0530

    changed variable for equalmethod code

--- src/practice/EqualMethod.java
+++ src/practice/EqualMethod.java
@@ -4,11 +4,11 @@ public class EqualMethod {
 
 	public static void main(String[] args) {
 		
-		String s1=new String("Radha");
-		String s2=new String("Radha");
+		String t1=new String("Radha");
+		String t2=new String("Radha");
 		
-		System.out.println(s1==s2); //false
-		System.out.println(s1.equals(s2)); //true
+		System.out.println(t1==t2); //false
+		System.out.println(t1.equals(t2)); //true
 
 	}
 
